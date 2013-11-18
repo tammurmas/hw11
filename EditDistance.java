a1a40d44f66d3f3410fe8f578e67c03b07bc0544
tammurmas <tammurmas@gmail.com>
2013-11-18 23:27:45 +0200

    comments to 2nd solution
    added some brief comments and a print function

--- EditDistance.java
+++ EditDistance.java
@@ -22,7 +22,7 @@ public class EditDistance {
     
   public static void main(String[] args)
   {
-      System.out.println(editDistance("abarabckakcadkcdabbaarra","barackadabadckdbaabararaa"));
+      System.out.println(editDistance("ab","baa"));
   }
     
   /**
@@ -34,25 +34,40 @@ public class EditDistance {
   public static int editDistance(String s, String t){
     int m=s.length();
     int n=t.length();
+    
     int[][]d=new int[m+1][n+1];
+    
     for(int i=0;i<=m;i++){
-      d[i][0]=i;
+        d[i][0]=i;
     }
+    
     for(int j=0;j<=n;j++){
-      d[0][j]=j;
+        d[0][j]=j;
     }
+    
     for(int j=1;j<=n;j++){
-      for(int i=1;i<=m;i++){
-        if(s.charAt(i-1)==t.charAt(j-1)){
-          d[i][j]=d[i-1][j-1];
-        }
-        else{
-          d[i][j]=min((d[i-1][j]+1),(d[i][j-1]+1),(d[i-1][j-1]+1));
-        }
+        for(int i=1;i<=m;i++){
+            if(s.charAt(i-1)==t.charAt(j-1)){
+                d[i][j]=d[i-1][j-1];//characters are equal, we don't have to change anything
+            }
+            else{
+                d[i][j]=min((d[i-1][j]+1),(d[i][j-1]+1),(d[i-1][j-1]+1));//take the minimal neighboring value
+            }
       }
     }
+    
+    for (int i=0; i<=m; i++)
+    {
+        for (int j=0; j<=n; j++)
+        {
+            System.out.print(d[i][j]+" ");
+        }
+        System.out.println();
+    }
+    
     return(d[m][n]);
   }
+  
   public static int min(int a,int b,int c){
     return(Math.min(Math.min(a,b),c));
   }
