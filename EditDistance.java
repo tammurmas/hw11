07bd438a889c943b7dcbdb36c01a851d60bf3cbc
tammurmas <tammurmas@gmail.com>
2013-11-20 12:00:27 +0200

    Helper for the 3rd
    3rd assignment is half-way done, added a small helpr to print out the
    difference matrix.

--- EditDistance.java
+++ EditDistance.java
@@ -22,7 +22,11 @@ public class EditDistance {
     
   public static void main(String[] args)
   {
-      System.out.println(editDistance("ab","baa"));
+      String s = "democrat";
+      String t = "republican";
+      
+      int[][] d = editDistance(s,t);
+      printMatrix(d,s,t);
   }
     
   /**
@@ -31,16 +35,18 @@ public class EditDistance {
    * @param t
    * @return 
    */
-  public static int editDistance(String s, String t){
+  public static int[][] editDistance(String s, String t){
     int m=s.length();
     int n=t.length();
     
     int[][]d=new int[m+1][n+1];
     
+    //fill in columns
     for(int i=0;i<=m;i++){
         d[i][0]=i;
     }
     
+    //fill in rows
     for(int j=0;j<=n;j++){
         d[0][j]=j;
     }
@@ -51,24 +57,48 @@ public class EditDistance {
                 d[i][j]=d[i-1][j-1];//characters are equal, we don't have to change anything
             }
             else{
-                d[i][j]=min((d[i-1][j]+1),(d[i][j-1]+1),(d[i-1][j-1]+1));//take the minimal neighboring value
+                d[i][j]=min((d[i-1][j]+1),(d[i][j-1]+1),(d[i-1][j-1]+1));//take the minimal neighbouring value
             }
       }
     }
     
-    for (int i=0; i<=m; i++)
-    {
-        for (int j=0; j<=n; j++)
-        {
-            System.out.print(d[i][j]+" ");
-        }
-        System.out.println();
-    }
     
-    return(d[m][n]);
+    
+    //return(d[m][n]);
+    return d;
   }
   
   public static int min(int a,int b,int c){
     return(Math.min(Math.min(a,b),c));
+  }
+  
+  public static void printMatrix(int[][] d, String s, String t)
+  {
+        int m=s.length();
+        int n=t.length();
+        
+        System.out.print(". ");
+        
+        for(int j=0;j<=n;j++){
+            if(j > 0)
+                System.out.print(t.charAt(j-1)+" ");
+            else
+                System.out.print(". ");
+        }
+        System.out.println();
+        
+        for (int i=0; i<=m; i++)
+        {
+            if(i > 0)
+                System.out.print(s.charAt(i-1)+" ");
+            else
+                System.out.print(". ");
+            
+            for (int j=0; j<=n; j++)
+            {
+                System.out.print(d[i][j]+" ");
+            }
+            System.out.println();
+        }
   }
 }
